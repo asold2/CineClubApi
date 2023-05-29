70eb2420995a36ac0b1812bd9b4a05b33cff2688
asold2 <304147@via.dk>
2023-05-29 20:55:30 +0200

    created some tests for creation of user

--- CineClubApiTest/Services/AccountServices/AuthServiceTest.cs
+++ CineClubApiTest/Services/AccountServices/AuthServiceTest.cs
@@ -1,7 +1,13 @@
-﻿namespace CineClubApiTest.Services.AccountServices
+﻿using CineClubApi.Common.Interfaces;
+using CineClubApi.Services.AccountService;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace CineClubApiTest.Services.AccountServices
 {
     public class AuthServiceTest
     {
-        
+        private readonly Mock<IApplicationDbContext>
     }
 }
